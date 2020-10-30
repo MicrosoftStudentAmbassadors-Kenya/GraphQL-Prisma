f0336e97415d5995a7547dd7261e45fb99f0fb50
ruheni <aleruheni@outlook.com>
2020-10-30 21:16:42 +0300

    updated typeDefs for mutation

--- src/typeDefs.js
+++ src/typeDefs.js
@@ -18,7 +18,7 @@ type Query {
 }
 
 type Mutation {
-    createBook(author: String, title: String!, description: String!, rating: Int, published: Boolean): Book!
+    createBook(author: String!, title: String!, description: String!, rating: Int!, published: Boolean!): Book!
     updateOneBook(id: ID!, title: String!, description: String, rating: Int, published: Boolean): Book
     deleteOneBook(id: ID!): Book
 }
